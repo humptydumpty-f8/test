eee5e27fe8d57960b9a3594e7d87b161b1e2bb86
Yu Tarnavski <george@aprodos.kpi.ua>
2020-12-29 10:56:41 +0200

    var4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }
